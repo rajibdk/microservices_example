af372a2e3711dbb4d37f37b10413541a1631d97a
Deka <Rajib.Deka@siemens.com>
2016-03-01 17:45:01 +0530

    First Commit

---
+++ order-repository/src/main/java/sample/service/ApplicationConfig.java
@@ -0,0 +1,24 @@
+package sample.service;
+
+import javax.inject.Named;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class ApplicationConfig {
+
+	@Named
+	static class JerseyConfig extends ResourceConfig {
+		public JerseyConfig() {
+			this.packages("sample.service.rest");
+		}
+	}
+	
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+}
---
+++ order-repository/src/main/java/sample/service/rest/Order.java
@@ -0,0 +1,42 @@
+package sample.service.rest;
+
+import java.util.Date;
+
+public class Order {
+	private long id;
+	private long amount;
+	private Date dateOfOrder;
+	private Customer customer;
+	private Product product;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public long getAmount() {
+		return amount;
+	}
+	public void setAmount(long amount) {
+		this.amount = amount;
+	}
+	public Date getDateOfOrder() {
+		return dateOfOrder;
+	}
+	public void setDateOfOrder(Date dateOfOrder) {
+		this.dateOfOrder = dateOfOrder;
+	}
+	public Customer getCustomer() {
+		return customer;
+	}
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+	public Product getProduct() {
+		return product;
+	}
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+}
